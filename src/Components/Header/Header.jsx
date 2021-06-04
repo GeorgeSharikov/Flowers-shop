04e68d37e8121b54d6ceb95a25de0d0b555cd9a3
George <g-sharikov@bk.ru>
2021-06-04 11:56:43 +0300

    new design

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,17 +5,25 @@ import {HeaderMenu} from "./HeaderMenu/HeaderMenu";
 import {Cart} from "./Cart/Cart";
 
 const HeaderWrapper = styled.div`
-  display: grid;
   grid-area: header;
-  grid-template-columns: 50px 100px 200px 1fr 250px;
+  grid-template-rows: 180px 50px;
+`
+const TopHeader = styled.div`
+  display: grid;
+  grid-template-columns: 50px 100px 100px 1fr 250px;
   align-items: center;
+  margin-top: 10px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #f0f0f0;
 `
 export const Header = (props) => {
     return (
         <HeaderWrapper>
-            <Logo />
-            <HeaderMenu />
-            <Cart />
+            <TopHeader >
+                <Logo />
+                <HeaderMenu />
+                <Cart />
+            </TopHeader>
         </HeaderWrapper>
     );
 }
--- src/Components/Header/HeaderMenu/HeaderMenu.jsx
+++ src/Components/Header/HeaderMenu/HeaderMenu.jsx
@@ -1,52 +1,64 @@
 import React from 'react';
 import styled from 'styled-components'
-import {NavLink} from "react-router-dom";
+import {CallOutlined, SearchOutlined} from '@material-ui/icons'
 
-const Menu = styled.div`
-  display: grid;
+const styles = {
+    fontSize: 25,
+    marginTop: 10,
+    marginRight: 10
+}
+
+const SearchBar = styled.div`
+  display: flex;
   grid-column: 4/5;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: 1fr;
+  margin-right: 15px;
+  justify-content: center;
+  position: relative;
+`
+const PhoneNumber = styled.div`
+    text-align: left;
+    width: 150px;
+    font-weight: 100;
+    margin-right: 20px;
+`
+const Input = styled.input`
+    margin-right: 50px;
+    width: 500px;
+    height: 45px;
+    border-radius: 50px;
+    border: 1px black solid;
+    outline: none;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 15px;
+`
+const Button = styled.button`
+    height: 45px;
+    width: 20px;
+    position: absolute;
+    background-color: transparent;
+    border: none;
+    padding: 0px;
+    right:90px;
+    top:3px;
+    cursor: pointer;
 `
 
-const MenuItem = styled(NavLink)`
-  text-decoration: none;
-  font-size: 20px;
-  color: black;
-  user-select: none;
-  transition: font-size .2s;
-  &.active{
-     color: darkcyan;
-     font-size: 24px;
-  }
-  &:hover{
-    font-size: 24px;
-  }
-`
 
 export const HeaderMenu = (props) => {
     return (
-        <Menu>
-            <div>
-                <MenuItem to={'/about'}>
-                    About
-                </MenuItem>
-            </div>
-            <div>
-                <MenuItem to={'/flowers'}>
-                    Flowers
-                </MenuItem>
-            </div>
-            <div>
-                <MenuItem to={'/deliver'}>
-                    Deliver
-                </MenuItem>
-            </div>
-            <div>
-                <MenuItem to={'/feedback'}>
-                    Feedback
-                </MenuItem>
-            </div>
-        </Menu>
+        <SearchBar>
+                <CallOutlined style={styles}/>
+                <PhoneNumber >
+                    PHONE: <br />      
+                    +7(909)-666-66-66
+                </PhoneNumber>
+                <form onClick={(e) => e.preventDefault()}>
+                    <Input placeholder={'Search'} maxLength='50'/>
+                    <Button>
+                            <SearchOutlined />              
+                    </Button>
+                </form>
+        </SearchBar>
     );
 }
