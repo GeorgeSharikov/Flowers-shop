777d4899bd4a43f8ad2ea76f64597721389cd205
George <g-sharikov@bk.ru>
2021-07-02 00:24:31 +0300

    add global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import  {createGlobalStyle} from 'styled-components'
@@ -8,10 +7,59 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux'
 import { store } from './redux/store';
 
-const Global = createGlobalStyle(`
-    margin: 0px;
-    padding: 0px;
-`)
+const Global = createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+ul[class],
+ol[class] {
+  padding: 0;
+}
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+ul[class],
+ol[class],
+li,
+figure,
+figcaption,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+}
+ul[class],
+ol[class] {
+  list-style: none;
+}
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+img {
+  max-width: 100%;
+  display: block;
+}
+article > * + * {
+  margin-top: 1em;
+}
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+`
 
 ReactDOM.render(
   <React.StrictMode>
