689d5ef34cef409c5de9eec2e10a194cd59d79a9
George <g-sharikov@bk.ru>
2021-07-04 13:34:01 +0300

    Added bonus images and made bonus-bar

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { MainSlider } from "./Slider/Slider"
+import {Bonuses} from "./Bonuses/Bonuses";
 
 export const About = () => {
      return <div>
          <MainSlider />
+         <Bonuses />
      </div>
 }
