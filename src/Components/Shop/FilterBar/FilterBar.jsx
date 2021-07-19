1441f4e0467e5221bb9de7feeb3d0db6930f9849
George <g-sharikov@bk.ru>
2021-07-19 21:12:49 +0300

    delete pageshow component and fix styles ind FilterBar.jsx

--- src/Components/Shop/FilterBar/FilterBar.jsx
+++ src/Components/Shop/FilterBar/FilterBar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
-import {PageShow} from "./PageShow";
 import {Sorting} from "./Sorting";
 
 const FilterWrapper = styled.div`
    display: flex;
-   justify-content: space-between;
+   justify-content: flex-end;
    align-items: center;
    @media(max-width: 420px){
       flex-direction: column;
@@ -18,7 +17,6 @@ const FilterWrapper = styled.div`
 export const FilterBar = (props) => {
     return (
             <FilterWrapper>
-                <PageShow />
                 <Sorting />
             </FilterWrapper>
     );
