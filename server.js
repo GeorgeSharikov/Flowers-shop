5eac822739e9b5c6a70b272b47f77650a47cccab
GeorgeSharikov <75853792+GeorgeSharikov@users.noreply.github.com>
2021-11-20 16:56:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use('https://my-pet-project-flowers-shop.herokuapp.com/', express.static( path.join(__dirname, "client", "build")))
+app.use(express.static( path.join(__dirname, "client", "build")))
 app.get('*', function (req, res) {
   if (req.method === 'GET' && req.accepts('html') && !req.is('json') && !req.path.includes('.')) {
     res.sendFile('index.html', { root })
