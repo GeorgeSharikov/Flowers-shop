2937972cab7162c22ca4359d4f37c6fb1079a4c7
George <g-sharikov@bk.ru>
2021-11-20 15:19:21 +0300

    fix bug

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ import express from 'express'
 import path from 'path'
 
 const __dirname = path.resolve()
+const root = path.join(__dirname, "client", "build",)
 const PORT = process.env.PORT || 3000
+
 const app = express()
 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
+app.use(express.static(root))
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile("index.html", {root});
 });
 
 app.listen(PORT, () => console.log('listening...'))
